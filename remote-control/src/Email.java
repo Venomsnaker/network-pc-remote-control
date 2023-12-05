4d9bd65446e4e83fb6374d5a7b6d1d8a8740f17e
pigpig <vantuankiet.work@gmail.com>
2023-12-05 18:12:34 +0700

    sendEmail + getEmail -> Email

--- remote-control/src/Email.java
+++ remote-control/src/Email.java
@@ -1,19 +1,22 @@
 import java.util.Date;
 import java.util.Properties;
-
+import javax.mail.Address;
+import javax.mail.Folder;
 import javax.mail.Authenticator;
 import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.NoSuchProviderException;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import javax.mail.Store;
+
 
 public class Email {
-    // app name: network-pc-remote-control
-    // email: g4.22tnt1.hcmus@gmail.com
-    // pass: xpfa bvas rrgb qmta
-    static final String from = "g4.22tnt1.hcmus@gmail.com";
+
+    static final String from  = "g4.22tnt1.hcmus@gmail.com";
     static final String password = "xpfabvasrrgbqmta";
 
     public static boolean sendEmail(String to, String tieuDe, String noiDung) {
@@ -59,10 +62,90 @@ public class Email {
         }
     }
 
-    public static void main(String[] args) {
-        for (int i = 0; i < 2; i++) {
-            Email.sendEmail("vantuankiet.hs@gmail.com", System.currentTimeMillis() + "", "Huy testing commit!");
+    private static Properties getServerProperties(String protocol, String host, String port) {
+        Properties props = new Properties();
+        props.put(String.format("mail.%s.host", protocol), host);
+        props.put(String.format("mail.%s.port", protocol), port);
+
+        props.setProperty(
+                String.format("mail.%s.socketFactory.class", protocol),
+                "javax.net.ssl.SSLSocketFactory");
+
+        props.setProperty(
+                String.format("mail.%s.socketFactory.fallback", protocol),
+                "false");
+
+        props.setProperty(
+                String.format("mail.%s.socketFactory.port", protocol),
+                String.valueOf(port));
+
+        return props;
+    }
+
+    public static String[] getInfo(String protocol, String host, String port, String userName, String password) {
+        String[] ans = {"", ""};
+
+        Properties props = getServerProperties(protocol, host, port);
+        Session session = Session.getDefaultInstance(props);
+
+        try {
+            Store store = session.getStore(protocol);
+            store.connect(userName, password);
+
+            Folder folderInbox = store.getFolder("INBOX");
+            folderInbox.open(Folder.READ_ONLY);
+
+            Message[] messages = folderInbox.getMessages();
+
+
+            Message msg = messages[messages.length - 1];
+            Address[] fromAddress = msg.getFrom();
+            String from = fromAddress[0].toString();
+            String subject = msg.getSubject();
+            String contentType = msg.getContentType();
+            String messageContent = "";
+
+            if (contentType.contains("text/html"))
+            {
+                try {
+                    Object content = msg.getContent();
+                    if (content != null) {
+                        messageContent = content.toString();
+                    }
+                } catch (Exception ex) {
+                    messageContent = "!Error downloading content";
+                    ex.printStackTrace();
+                }
+            }
+            ans[0] = subject;
+            ans[1] = from;
         }
+        catch (NoSuchProviderException ex) {
+            System.out.println("No provider for protocol " + protocol);
+            ex.printStackTrace();
+        }
+        catch (MessagingException ex) {
+            System.out.println("Could not connect to the message store");
+            ex.printStackTrace();
+        }
+
+        return ans;
+    }
+
+
+    public static void main(String[] args) {
+        String protocol = "imap";
+        String host = "imap.gmail.com";
+        String port = "993";
+
+        String username = "g4.22tnt1.hcmus@gmail.com";
+        String password = "xpfabvasrrgbqmta";
+
+//        Email.sendEmail("vantuankiet.hs@gmail.com", "try", "Day la noi dung thu");
+
+        String[] tmp =  Email.getInfo(protocol, host, port, username, password);
+        System.out.println(tmp[0]);
+        System.out.println(tmp[1]);
 
     }
 
